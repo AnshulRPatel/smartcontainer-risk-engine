0e37ee87374f3803e3a9a44c4f08026ab50558ef
Anshul Patel <anshul.gujarat@gmail.com>
2026-06-14 18:19:13 +0530

    Fix Streamlit module path resolution

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
+
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        ".."
+    )
+)
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.append(PROJECT_ROOT)
+
+
+
 from dashboard.utils.data_loader import (
     load_dashboard_data
 )
